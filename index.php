0e476c1dd63ae16bee52ac25438e75b1d521dd8c
Jiwon Jennie Jeon <59095617+jeonsupport@users.noreply.github.com>
2022-05-26 17:22:44 +0900

    홈화면 수정

--- index.php
+++ index.php
@@ -1,82 +1,94 @@
 <!DOCTYPE html>
 <html lang="ko">
 <?php include('layout/header.php');?>
-<section class="main_wrap">
-    <div class="subTopBox inner">
-        <div class="swiper-container mySwiper">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <div class='sw sw03'><img src="assets/img/main03.png">
-                        <div class="btnBox">
-                            <a href="https://play.google.com/store/apps/details?id=com.wellcomad.multipay" target='_blank'><img src="assets/img/ad/ad_googleplay.png"></a>
-                            <a href="https://apps.apple.com/app/%EB%A9%80%ED%8B%B0%ED%8E%98%EC%9D%B4/id1611127090?platform=iphone" target='_blank'><img src="assets/img/ad/ad_appstore.png"></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <a href="guide_qr.php">
-                    <div class='sw sw01'><img src="assets/img/main01.jpg"></div>
-                    </a>
-                </div>
-                <!-- <div class="swiper-slide">
-                    <a href="shop.php">
-                    <div class='sw sw02'><img src="assets/img/main02.jpg"></div>
-                    </a>
-                </div> -->
-                <div class="swiper-slide">
-                    <a href="guide_bank.php">
-                    <div class='sw sw04'><img src="assets/img/main04.jpg"></div>
-                    </a>
-                </div>
-            </div>
+<section class="cont home">
+    <div class="banner"><img src="assets/img/img01.gif"></div>
+    <div class="inner">
+        <h1>멀티페이의 인기 상품권</h1>
+        <h2><b>HOT</b> 인기 상품권</h2>
+        <div class="contTicket">
+            <ul>
+                <li>
+                    <div class="ico best"><img src="assets/img/i_best.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1006.png"></div>
+                </li>
+                <li>
+                    <div class="ico best"><img src="assets/img/i_best.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1008.png"></div>
+                </li>
+            </ul>
+        </div>
+        <h1>멀티페이의 베스트셀러 상품권</h1>
+        <h2>BEST SELLER</h2>
+        <div class="contTicket">
+            <ul>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1008.png"></div>
+                    <p><span>온라인전용</span>문화상품권</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1006.png"></div>
+                    <p><span>온라인전용</span>해피머니상품권</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1007.png"></div>
+                    <p><span>온라인전용</span>도서문화상품권</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1002.png"></div>
+                    <p><span>온라인전용</span>퍼니카드</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1014.png"></div>
+                    <p><span>온라인전용</span>넥슨캐시</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1010.png"></div>
+                    <p><span>온라인전용</span>틴캐시</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1011.png"></div>
+                    <p><span>온라인전용</span>에그머니</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1023.png"></div>
+                    <p><span>온라인전용</span>구글기프트카드</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1022.png"></div>
+                    <p><span>온라인전용</span>카카오코인</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1009.png"></div>
+                    <p><span>온라인전용</span>스마트문상</p>
+                </li>
+                <li>
+                    <div class="ico multiQr"><img src="assets/img/ic_qr.png"></div>
+                    <div class="imgTicket"><img src="assets/img/basket/1019.png"></div>
+                    <p><span>온라인전용</span>통화상품권</p>
+                </li>
+                <li class="none"><a href="shop.php"><div class="imgTicket"><img src="assets/img/basket/0000.png"></div></a></li>
+            </ul>
         </div>
     </div>
-    <div class="cont inner">
-        <ul>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div>
-                <a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1008.png'></div><p>[온라인전용] 문화상품권<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1006.png'></div><p>[온라인전용] 해피머니상품권<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1007.png'></div><p>[온라인전용] 도서문화상품권<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1002.png'></div><p>[온라인전용] 퍼니카드<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1014.png'></div><p>[온라인전용] 넥슨캐시<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1010.png'></div><p>[온라인전용] 틴캐시<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1011.png'></div><p>[온라인전용] 에그머니<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1023.png'></div><p>[온라인전용] 구글플레이기프트카드<br><span>상품권종</span><span>3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1022.png'></div><p>[온라인전용] 카카오코인<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1009.png'></div><p>[온라인전용] 스마트문상<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li><div class='ic_qr'><img src='assets/img/ic_qr.png'></div><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/1019.png'></div><p>[온라인전용] 통화상품권<br><span>상품권종</span><span>1천/3천/5천/1만/3만/5만원</span></p>
-            <!-- <div class="fee_box"><p>수수료</p><h3><strong>할인</strong></h3></div> -->
-            </a></li>
-            <li class="none"><a href='shop.php'><div class="gift_img"><img src='assets/img/basket/0000.png'></div></a></li>
-        </ul>
-    </div>
+
     <div class="cont02 admulti">
         <h3>멀티페이 앱이 출시됐어요</h3>
         <h5>상품권 구매는 <b>반드시</b> APP다운로드 및 설치해주세요</h5>
         <a href="https://apps.apple.com/app/%EB%A9%80%ED%8B%B0%ED%8E%98%EC%9D%B4/id1611127090?platform=iphone" target="_blank"><img src='assets/img/ad/ad_appstore.png'></a>
         <a href="https://play.google.com/store/apps/details?id=com.wellcomad.multipay" target="_blank"><img src='assets/img/ad/ad_googleplay.png'></a>
     </div>
-    <div class="cont01" id="howInfo">
+    <!-- <div class="cont01" id="howInfo">
         <div class='info'>
             <h1>이용안내</h1>
         </div>
@@ -109,23 +121,23 @@
                 </li>
             </ul>
         </div>
-    </div>
+    </div> -->
 </section>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script>
     AOS.init();
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 100,
-        loop: true,
-        loopAdditionalSlides: 1,
-        autoplay: {
-            delay: 6000,
-            disableOnInteraction: false,
-        },
-        pagination: false,
-    });
+    // var swiper = new Swiper(".mySwiper", {
+    //     slidesPerView: 1,
+    //     spaceBetween: 100,
+    //     loop: true,
+    //     loopAdditionalSlides: 1,
+    //     autoplay: {
+    //         delay: 6000,
+    //         disableOnInteraction: false,
+    //     },
+    //     pagination: false,
+    // });
 </script>
 <?php include('layout/footer.php');?>
 </html>
