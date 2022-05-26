c0381ea2f0066ce68452cb9b872f2ba0223eb83e
Jiwon Jennie Jeon <59095617+jeonsupport@users.noreply.github.com>
2022-05-26 12:47:12 +0900

    이용/구매안내 페이지 ui

--- guide.php
+++ guide.php
@@ -3,17 +3,20 @@
 <?php include('layout/header.php');?>
 <section class="cont guide">
     <div class="contTitle">
-        <a href="guide.php" class="act">
-        이용안내
-        </a>
-        <a href="guide_qr.php" class="">
-        QR구매안내
-        </a>
+        <a href="guide.php" class="act">이용안내</a>
+        <a href="guide_qr.php" class="">QR구매안내</a>
     </div>
     <p>멀티페이는 안전하고 편리한 온라인 상품권 구매 관리 서비스를 제공하고 있습니다</p>
-
     <div class="contSubmit">
-        <div class="chap01">
+        <div class="chap chap01">
+            <div class="chapTxt">
+            <p>이용안내</p>
+            <p><span>SNS구매</span> 혹은 QR구매를 통해<br>상품권을 편리하고 다양하게 보관/사용할 수 있는 구매 방법입니다</p>
+            <b>현재 QR구매만 가능</b>
+            <p><b>반드시</b> 멀티페이 APP이 설치되어 있어야 이용 가능합니다</p>
+            <a href=""><img src="assets/img/ad/ad_appstore.png"></a>
+            <a href=""><img src="assets/img/ad/ad_googleplay.png"></a>
+            </div>
             <img src="assets/img/guideA.png">
         </div>
     </div>
--- guide_qr.php
+++ guide_qr.php
@@ -1,35 +1,36 @@
 <!DOCTYPE html>
 <html lang="ko">
 <?php include('layout/header.php');?>
-<section class="sub_bo_Wrap">
-    <!-- <div class="subT_SideBox">
-        <div class="subtName">
-            <h1>상품권 QR 구매 안내</h1>
-            <h2>상품권 QR 구매하여 상품권을 편리하게 관리할 수 있습니다</h2>
-        </div>
-    </div> -->
-    <div id="process_submit">
-        <div class="process_sideBox">
-            <div class="top_phoneBox"></div>
-            <div class="oi_inputArea">
-                <div class="recentTableBox guide">
-                    <h4><b>QR 구매란?</b><br><br>QR코드로 발권하여 상품권을 편리하고 다양하게 보관/사용할 수 있는 구매 방법입니다</h4>
-                    <div class="cont02 admulti">
-                        <h5><b>반드시</b> 멀티페이 앱이 설치되어 있어야 이용이 가능합니다</h5>
-                        <a href="https://apps.apple.com/app/%EB%A9%80%ED%8B%B0%ED%8E%98%EC%9D%B4/id1611127090?platform=iphone" target="_blank"><img src='assets/img/ad/ad_appstore.png'></a>
-                        <a href="https://play.google.com/store/apps/details?id=com.wellcomad.multipay" target="_blank"><img src='assets/img/ad/ad_googleplay.png'></a>
-                    </div>
-                    <p><span>1.</span><a>상품권 구매 페이지</a>에서 원하는 상품권을 구매하세요</p>
-                    <img src='assets/img/guide01.png'>
-                    <p><span>2.</span>입금계좌를 확인하고 해당 계좌로 <a>입금</a>해주세요</p>
-                    <img src='assets/img/guide02.png'>
-                    <p><span>3.</span>입금이 완료되면 실시간으로 QR코드 이미지가 생성됩니다<br><a>[APP에서 상품권 내려받기] 버튼</a> 혹은 <a>멀티페이 APP</a> 내에서 QR코드를 스캔해주세요</p>
-                    <img src='assets/img/guide03.png'>
-                    <p><span>4.</span>멀티페이 APP에 <a>구매한 상품권</a>이 등록됩니다</p>
-                    <img src='assets/img/guide04.png'>
-                </div>
+<section class="cont guide">
+    <div class="contTitle">
+        <a href="guide.php" class="">이용안내</a>
+        <a href="guide_qr.php" class="act">QR구매안내</a>
+    </div>
+    <p>멀티페이는 안전하고 편리한 온라인 상품권 구매 관리 서비스를 제공하고 있습니다</p>
+    <div class="contSubmit">
+        <div class="chap chap02">
+            <div class="chapTxt">
+            <p>QR구매안내</p>
+            <p><span>QR구매</span>는 QR코드로 발권하여<br>상품권을 편리하고 다양하게 보관/사용할 수 있는 구매 방법입니다</p>
+            <p><b>반드시</b> 멀티페이 APP이 설치되어 있어야 이용 가능합니다</p>
+            <a href=""><img src="assets/img/ad/ad_appstore.png"></a>
+            <a href=""><img src="assets/img/ad/ad_googleplay.png"></a>
             </div>
+            <img src="assets/img/guideB.png">
         </div>
+        <div class="chapNav">
+            <a href="" class="act">PC</a>
+            <a href="">MOBILE</a>
+        </div>
+        <ul class="chapExplain">
+            <li><b>1</b>상품권 구매 페이지에서 원하시는 상품권을 구매해주세요.<br>입금정보를 기억하시면 구매 후 언제든지 구매내역을 조회하실 수 있습니다.
+        <img src="assets/img/guide01.png">
+        </li>
+            <li><b>2</b>입금 계좌 안내를 확인하고 해당 계좌로 입금해주세요.<br>입금 전, 팝업을 닫을 시 구매신청이 자동으로 취소됩니다. 입금 확인이 될 때까지 팝업을 닫지 않도록 주의하세요.</li>
+            <li><b>3</b>입금이 완료되면 실시간으로 QR코드 이미지가 생성됩니다.<br>멀티페이 APP에서 해당 QR코드를 스캔해주세요.</li>
+            <li><b>4</b>멀티페이 APP에 구매한 상품권이 등록됩니다.</li>
+            <li><b>3</b></li>
+        </ul>
     </div>
 </section>
 <?php include('layout/footer.php');?>
