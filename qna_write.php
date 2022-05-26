d45980f559b7092c1d318807f5b52acbbf534ba9
Jiwon Jennie Jeon <59095617+jeonsupport@users.noreply.github.com>
2022-05-26 10:14:53 +0900

    문의사항 디테일/문의 ui

--- qna_write.php
+++ qna_write.php
@@ -1,58 +1,43 @@
 <!DOCTYPE html>
 <html lang="ko">
 <?php include('layout/header.php');?>
-<section class="sub_bo_Wrap">
-    <div class="subT_SideBox">
-        <div class="subtName">
-            <h1>문의사항</h1>
-            <h2>언제든지 고객센터로 연락주시면 신속히 해결해드리겠습니다</h2>
-        </div>
+<section class="cont inner">
+    <div class="contTitle">
+        <h1>문의사항</h1>
+        <h2>언제든지 고객센터로 연락주시면 신속히 해결해드리겠습니다</h2>
     </div>
-    <div id="process_submit">
-        <div class="process_sideBox">
-            <!-- <div class="top_findBox">QnA 글쓰기</div> -->
-            <div class="oi_inputArea">
-                <div class="recentTableBox">
-                    <section class="con">
-                        <div class="board">
-                            <div class="t_view">
-                                <form name="form" method="post" onsubmit="return wrSend()" action="./action/write_action.php">
-                                    <input type="hidden" name="kind" value="user" />
-                                    <table>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">제목</th>
-                                                <td><input type="text" class="form-control cs-a" placeholder="제목" name="title" oninput="maxLengthCheck(this)" maxlength='50'></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">작성자</th>
-                                                <td><input type="text" class="form-control cs-a" placeholder="작성자" name="name" oninput="maxLengthCheck(this)" maxlength='10'></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">내용</th>
-                                                <td><textarea class="form-control" rows="3" name="contents" oninput="maxLengthCheck(this)" maxlength='1500'></textarea></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">비밀번호</th>
-                                                <td><input type="password" class="form-control cs-a" placeholder="4자리 입력" oninput="maxLengthCheck(this)" maxlength='4' name="pw"></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><img src="captcha.php?date=<?echo date('h:i:s')?>"></th>
-                                                <td><input type="text" class="form-control cs-a" placeholder="왼쪽 이미지의 글자를 입력하세요." oninput="maxLengthCheck(this)" maxlength='5' name="captcha"></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                    <div class="btnArea">
-                                        <button type="submit" class="btn btn-primary fr">저장</button>
-                                        <button type="button" class="btn btn-outline-primary fr" onclick="location.href='qna.php'">취소</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </section>
-                </div>
+    <div class="contSubmit">
+        <div class="writeBox box writeBoxQna">
+            <form name="form" method="post" onsubmit="return wrSend()" action="./action/write_action.php">
+                <input type="hidden" name="kind" value="user" />
+                <table>
+                    <tr>
+                        <th>제목</th>
+                        <td><input type="text" placeholder="제목을 입력해주세요" name="title" oninput="maxLengthCheck(this)" maxlength='50'></td>
+                    </tr>
+                    <tr>
+                        <th>작성자</th>
+                        <td><input type="text" placeholder="작성자를 입력해주세요" name="name" oninput="maxLengthCheck(this)" maxlength='10'></td>
+                    </tr>
+                    <tr>
+                        <th>내용</th>
+                        <td><textarea rows="3" placeholder="내용을 입력해주세요" name="contents" oninput="maxLengthCheck(this)" maxlength='1500'></textarea></td>
+                    </tr>
+                    <!-- <tr> -->
+                        <!-- <th>비밀번호</th> -->
+                        <!-- <td><input type="password" placeholder="4자리 입력" oninput="maxLengthCheck(this)" maxlength='4' name="pw"></td> -->
+                    <!-- </tr> -->
+                    <tr>
+                        <th><img src="captcha.php?date=<?echo date('h:i:s')?>"></th>
+                        <td><input type="text" placeholder="왼쪽 이미지의 텍스트를 입력해주세요" oninput="maxLengthCheck(this)" maxlength='5' name="captcha"></td>
+                    </tr>
+                </table>
             </div>
-        </div>
+            <div class="btnBox">
+                <button type="button" onclick="location.href='qna.php'">취소</button>
+                <button type="submit" class="color02">저장</button>
+            </div>
+        </form>
     </div>
 </section>
 <?php include('layout/footer.php');?>
